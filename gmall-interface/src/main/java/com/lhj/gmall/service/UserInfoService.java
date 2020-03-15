6f5cb48e08daae52580747ffb423576507ad5325
lihaoji <1224117846@qq.com>
2020-03-15 22:21:43 +0800

    小demo测试项目

---
+++ gmall-interface/src/main/java/com/lhj/gmall/service/UserInfoService.java
@@ -0,0 +1,15 @@
+package com.lhj.gmall.service;
+
+import com.lhj.gmall.bean.UserInfo;
+
+import java.util.List;
+
+/**
+ * @author lihaoji
+ * @create 2020-03-15 21:38
+ */
+public interface UserInfoService {
+
+    List<UserInfo> getUserInfoList();
+
+}
